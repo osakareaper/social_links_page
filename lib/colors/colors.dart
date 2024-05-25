8d5414b37a32fcd09e05ee859a33d85fc1fcb643
osakareaper <osakareaper@gmail.com>
2024-05-25 11:34:08 -0300

    feat: added photo gallery

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color darkRed = Color(0xFFa4223a);
+  static const Color bg1 = Color(0xFF09090A);
+  static const Color bg2 = Color(0xFF1A1A1F);
+  static const Color bg3 = Color(0xFF26262E);
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -9,38 +9,117 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final PageController _pageController = PageController();
+  final List<String> _imagePaths = [
+    'assets/profile_gallery/photo1.jpg',
+    'assets/profile_gallery/photo2.jpg',
+    'assets/profile_gallery/photo3.jpg',
+  ];
+
+  int _currentPage = 0;
+
+  void _nextPage() {
+    if (_currentPage < _imagePaths.length - 1) {
+      setState(() {
+        _currentPage++;
+        _pageController.animateToPage(
+          _currentPage,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      });
+    }
+  }
+
+  void _previousPage() {
+    if (_currentPage > 0) {
+      setState(() {
+        _currentPage--;
+        _pageController.animateToPage(
+          _currentPage,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: AppColors.bg2,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/profile_pic.jpg'),
+            // Profile Picture
+            const Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 8.0),
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/profile_pic.jpg'),
+              ),
             ),
-            SizedBox(height: 10),
-            Text(
+
+            // Display Name
+            const Text(
               'MARCUS OSAKA',
               style: TextStyle(
                 color: AppColors.darkRed,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20),
-            ListTile(
-              leading: Icon(Icons.arrow_forward_ios, color: AppColors.darkRed),
-              title: Text(
-                'EAE PORRA',
-                style: TextStyle(color: Colors.white),
+
+            // Username
+            const Text(
+              '@osakamarcus',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 12,
               ),
-              tileColor: Color.fromARGB(255, 22, 22, 22),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-            )
+            ),
+
+            // Photo Gallery
+            Expanded(
+                child: Stack(
+              children: [
+                PageView.builder(
+                  controller: _pageController,
+                  itemCount: _imagePaths.length,
+                  itemBuilder: (context, index) {
+                    return Image.asset(_imagePaths[index]);
+                  },
+                  onPageChanged: (index) {
+                    setState(() {
+                      _currentPage = index;
+                    });
+                  },
+                ),
+                Positioned(
+                  left: 10,
+                  top: MediaQuery.of(context).size.height / 2 - 25,
+                  child: IconButton(
+                    onPressed: _previousPage,
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 10,
+                  top: MediaQuery.of(context).size.height / 2 - 25,
+                  child: IconButton(
+                    onPressed: _nextPage,
+                    icon: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ))
           ],
         ),
       ),
